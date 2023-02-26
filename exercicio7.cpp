f85126b4e6c43f52931f0ac666f8eae910194ed9
Ronnie Santos <115759491+RonnieVip@users.noreply.github.com>
2023-02-25 22:30:08 -0300

    Add files via upload

---
+++ exercicio7.cpp
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	float;
+	printf("Informe o salário base: %f\n");
+	scanf("%f",&salario_base);
+	
+	
+	salario_receber=(salario_base*0.9);
+	
+	salario_receber=(salario_receber +600.00)
+
+	
+	
+	printf("Salário a receber : %.2f\n",salario_receber);
+	
+	
+	return 0;
+}
